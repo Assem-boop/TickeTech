6b832ef188c245e1ab6078ae8d02b99d536767f1
Ibrahim Darwish <ibrahim.darwish2005@gmail.com>
2025-04-19 17:23:14 +0200

    Update events.js #2

--- routes/events.js
+++ routes/events.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const eventController = require("../controllers/eventController");
 const { authMiddleware, authorizeRoles } = require("../middleware/auth");
 
-router.get("/", eventController.getAllEvents);
+router.get("/", eventController.getApprovedEvents);
+router.get("/all", authMiddleware, authorizeRoles("admin"), eventController.getAllEvents);
 router.get("/:id", eventController.getEventById);
 
 router.post("/", authMiddleware, authorizeRoles("organizer"), eventController.createEvent);
-router.put("/:id", authMiddleware, authorizeRoles("organizer", "admin"), eventController.updateEvent);
-router.delete("/:id", authMiddleware, authorizeRoles("organizer", "admin"), eventController.deleteEvent);
 
+router.put("/:id", authMiddleware, authorizeRoles("organizer", "admin"), eventController.updateEvent);
 router.put("/:id/status", authMiddleware, authorizeRoles("admin"), eventController.updateStatus);
+
+router.delete("/:id", authMiddleware, authorizeRoles("organizer", "admin"), eventController.deleteEvent);
 
 module.exports = router;
