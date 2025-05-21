cb086e7554a41b9c3b26115d45bd4565e885a9c8
AlyWaleed <aly.waleed@student.giu-ui.de>
2025-05-21 23:20:22 +0300

    fixes

--- routes/events.js
+++ routes/events.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const eventController = require("../controllers/eventController");
-const { authMiddleware, authorizeRoles } = require("../middleware/auth");
+const { protect, authorizeRoles } = require("../middleware/auth");
+
+router.post("/", protect, authorizeRoles("Organizer"), eventController.createEvent);
 
 router.get("/", eventController.getApprovedEvents);
-router.get("/all", authMiddleware, authorizeRoles("admin"), eventController.getAllEvents);
+
+router.get("/all", protect, authorizeRoles("Admin"), eventController.getAllEvents);
+
 router.get("/:id", eventController.getEventById);
 
-router.post("/", authMiddleware, authorizeRoles("organizer"), eventController.createEvent);
+router.put("/:id", protect, authorizeRoles("Organizer", "Admin"), eventController.updateEvent);
+router.delete("/:id", protect, authorizeRoles("Organizer", "Admin"), eventController.deleteEvent);
 
-router.put("/:id", authMiddleware, authorizeRoles("organizer", "admin"), eventController.updateEvent);
-router.put("/:id/status", authMiddleware, authorizeRoles("admin"), eventController.updateStatus);
-
-router.delete("/:id", authMiddleware, authorizeRoles("organizer", "admin"), eventController.deleteEvent);
+router.put("/:id/status", protect, authorizeRoles("Admin"), eventController.updateStatus);
 
 module.exports = router;
