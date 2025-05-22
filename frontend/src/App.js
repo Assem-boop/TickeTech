ec5f909835deea4487205ce59581928751aff10d
IbrahimDarwish <ibrahim.darwish2005@gmail.com>
2025-05-23 02:26:22 +0300

    fixing compiling bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import AdminDashboard from "./pages/AdminDashboard";
-import OrganizerDashboard from "./pages/OrganizerDashboard";
-import OrganizerMyEvents from "./pages/OrganizerMyEvents";
-import Bookings from "./pages/Bookings";
-import BookingDetails from "./pages/BookingDetails";
-import CreateEvent from "./pages/CreateEvent";
-import EditEvent from "./pages/EditEvent";
 import SendOtp from "./pages/SendOtp";
 import VerifyOtp from "./pages/VerifyOtp";
 import ResetPassword from "./pages/ResetPassword";
-import UserProfile from "./pages/UserProfile";
-import UserDetails from "./pages/UserDetails";
-import AdminUsersPage from "./pages/AdminUsersPage";
-import AdminEventsPage from "./pages/AdminEventsPage";
-import EventDetails from "./pages/EventDetails";
-import AllEventsPage from "./pages/AllEventsPage"; // ✅ NEW
 
 // Components
 import Navbar from "./components/Navbar";
@@ -39,124 +26,6 @@ function App() {
         <Route path="/forgot-password" element={<SendOtp />} />
         <Route path="/verify-otp" element={<VerifyOtp />} />
         <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/events" element={<AllEventsPage />} /> {/* ✅ NEW */}
-        <Route path="/events/:id" element={<EventDetails />} />
-
-        {/* 👤 Authenticated Profile */}
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <UserProfile />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* 🛠️ Admin Routes */}
-        <Route
-          path="/admin-dashboard"
-          element={
-            <ProtectedRoute>
-              <RoleRoute allowedRoles={["Admin"]}>
-                <AdminDashboard />
-              </RoleRoute>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/users"
-          element={
-            <ProtectedRoute>
-              <RoleRoute allowedRoles={["Admin"]}>
-                <AdminUsersPage />
-              </RoleRoute>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/user/:id"
-          element={
-            <ProtectedRoute>
-              <RoleRoute allowedRoles={["Admin"]}>
-                <UserDetails />
-              </RoleRoute>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/events"
-          element={
-            <ProtectedRoute>
-              <RoleRoute allowedRoles={["Admin"]}>
-                <AdminEventsPage />
-              </RoleRoute>
-            </ProtectedRoute>
-          }
-        />
-
-        {/* 🧾 Organizer Routes */}
-        <Route
-          path="/organizer-dashboard"
-          element={
-            <ProtectedRoute>
-              <RoleRoute allowedRoles={["Organizer"]}>
-                <OrganizerDashboard />
-              </RoleRoute>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/organizer-my-events"
-          element={
-            <ProtectedRoute>
-              <RoleRoute allowedRoles={["Organizer"]}>
-                <OrganizerMyEvents />
-              </RoleRoute>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/create-event"
-          element={
-            <ProtectedRoute>
-              <RoleRoute allowedRoles={["Organizer"]}>
-                <CreateEvent />
-              </RoleRoute>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/edit-event/:id"
-          element={
-            <ProtectedRoute>
-              <RoleRoute allowedRoles={["Organizer"]}>
-                <EditEvent />
-              </RoleRoute>
-            </ProtectedRoute>
-          }
-        />
-
-        {/* 🎟️ Standard User Bookings */}
-        <Route
-          path="/bookings"
-          element={
-            <ProtectedRoute>
-              <RoleRoute allowedRoles={["Standard"]}>
-                <Bookings />
-              </RoleRoute>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/bookings/:id"
-          element={
-            <ProtectedRoute>
-              <RoleRoute allowedRoles={["Standard"]}>
-                <BookingDetails />
-              </RoleRoute>
-            </ProtectedRoute>
-          }
-        />
       </Routes>
     </Router>
   );
